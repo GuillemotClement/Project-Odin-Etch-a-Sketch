f32a2e08832041c4e196bc93d6c79bf860104b16
GuillemotClement <guillemot.clement@protonmail.com>
2024-11-02 18:31:48 +0100

    Génération nouvelle grille via le bouton

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 const container = document.querySelector("#container");
 
-
-function getGrid(){
-    let nbRow = 16;
-
+/**
+ * Création de la grille
+ */
+function getGrid(value){
+    let nbRow = value;
     for (let i = 0; i < nbRow; i++){
         const row = document.createElement("div");
         container.appendChild(row);
         row.classList = "row";
-        createRow(row);
+        createRow(row, nbRow);
     }
 }
 
-function createRow(row){
-    let nbSquare = 16;
-    for(let i = 0; i < 16; i++){
+/**
+ * Création des carrés
+ * @param row
+ */
+function createRow(row, value){
+    for(let i = 0; i < value; i++){
         const square = document.createElement("div");
         square.classList = "square";
         row.appendChild(square);
     }
 }
 
-getGrid();
+
+// ------------------------ SAISIE USER -------------------------------------------------------------------------------->
+function getInput(){
+    container.innerHTML = "";
+    let isValid = false;
+    while (!isValid){
+        const value = prompt("Dimension ?");
+        if(value < 20){
+            getGrid(value);
+            isValid = true;
+        }else{
+            alert("La grille est trop grande");
+        }
+    }
+}
+
+const button = document.querySelector("#inputBtn");
+
+button.addEventListener("click", getInput);
